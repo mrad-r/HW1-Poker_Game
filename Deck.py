076f00a95561aedea1a80a9d03aea34c2338f8fa
matilderibeiro <matilde.csribeiro@gmail.com>
2025-03-31 23:18:27 +0200

    Poker game

--- Deck.py
+++ Deck.py
@@ -3,63 +3,60 @@ import random
 class Card:
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     SUITS = ["♣", "♦", "♥", "♠"]
-    # SUITS = ["clubs", "diamonds", "hearts", "spades"]
-    def __init__(self, rank, suit):
+    def __init__(self, suit, rank):
         if rank not in self.RANKS:
-            raise ValueError("Invalid Rank")
+            raise ValueError("Invalid rank")
         if suit not in self.SUITS:
-            raise ValueError("Invalid Suit")
-        self._rank = rank
+            raise ValueError("Invalid suit")
         self._suit = suit
+        self._rank = rank
 
-    @property
-    def rank(self):
-        return self._rank
+
+    def __gt__(self, other):
+        return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
+
+    def __str__(self):
+        return f"{self._rank}{self._suit}"
+
+    def __eq__(self, other):
+        return self.rank == other.rank
+
+    def __lt__(self, other):
+        return self.RANKS.index(self.rank) < self.RANKS.index(other.rank)
+
+    def __repr__(self):
+        return self.__str__()
 
     @property
     def suit(self):
         return self._suit
 
-    def __str__(self):
-        return f"{self._rank}{self._suit}"
-
-    def __repr__(self):
-        return self.__str__() # repr is the same as str
+    @property
+    def rank(self):
+        return self._rank
 
 class Deck:
     def __init__(self):
-        _cards = []
+        self._deck = []
         for suit in Card.SUITS:
             for rank in Card.RANKS:
-                _cards.append(Card(rank, suit))
-            self._cards = _cards
-
-    @property
-    def cards(self):
-        return self._cards
+                self._deck.append(Card(suit, rank))
 
     def __str__(self):
-        return str(self._cards)
+        return str(self._deck)
 
     def shuffle(self):
-        random.shuffle(self.cards)
+        random.shuffle(self._deck)
 
     def deal(self):
-        return self.cards.pop(0)
+        return self._deck.pop(0)
 
 if __name__ == "__main__":
-    c1 = Card("A", "♣")
-    print(c1)
-    c1._suit = "♥"
-    print(c1._suit, c1.rank)
-
     deck = Deck()
     print(deck)
     deck.shuffle()
     print(deck)
     print(deck.deal())
-    print(deck)
-
 
 
 
--- Game.py
+++ Game.py
@@ -1,13 +1,12 @@
-from turtledemo.minimal_hanoi import hanoi
-
 from Deck import Deck, Card
 
-class Hand:
+class PokerHand:
     def __init__(self, deck):
+
         cards = []
         for i in range(5):
             cards.append(deck.deal())
-            self._cards = cards
+        self._cards = cards
 
     @property
     def cards(self):
@@ -23,12 +22,77 @@ class Hand:
                 return False
         return True
 
-while True:
+    @property
+    def is_full_house(self):
+        return self.number_matches == 8
+
+    @property
+    def number_matches(self):
+        matches = 0
+        for i in range(len(self.cards)):
+            for j in range(len(self.cards)):
+                if i == j:
+                    continue
+                if self.cards[i].rank == self.cards[j].rank:
+                    matches += 1
+        return matches
+
+    @property
+    def is_pair(self):
+        if self.number_matches == 2: # simple
+            return True
+        return False
+
+    @property
+    def is_two_pair(self):
+        return self.number_matches == 4 # more advanced
+
+    @property
+    def is_trips(self):
+        if self.number_matches == 6:
+            return True
+        return False
+
+    @property
+    def is_full_house(self):
+        if self.number_matches == 8:
+            return True
+        else:
+            return False
+
+    @property
+    def is_quads(self):
+        if self.number_matches == 12:
+            return True
+        return False
+
+    @property
+    def is_straight(self):
+        """
+        Checks if the hand is straight
+        :return:
+        """
+        if self.number_matches != 0:
+            return False
+        self.cards.sort()
+        if Card.RANKS.index(self.cards[-1].rank) != Card.RANKS.index(self.cards[0].rank)+4:
+            return False
+        return True
+
+
+count = 0
+matches = 0
+while matches < 1000:
     deck = Deck()
     deck.shuffle()
-    hand = Hand(deck)
-    if hand.is_flush:
-        print(hand)
+    hand = PokerHand(deck)
+    if hand.is_straight:
+
+        matches += 1
+        # print(hand)
         break
+    count += 1
+
+print(f"probability of a full house is {100*matches/count}%")
 
 
